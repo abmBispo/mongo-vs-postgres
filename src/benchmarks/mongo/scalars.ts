d41621a4ed37b88d95af645545b8748042ad1e26
Alan Bispo <sr.alan.bispo@gmail.com>
2023-08-24 11:31:21 -0300

    wip

--- src/benchmarks/mongo/scalars.ts
+++
@@ -1,8 +0,0 @@
-const scalarTest = async (mongo) => {
-  const people = await mongo
-    .db("document_db")
-    .collection("people")
-    .findOne({ age: 32 });
-};
-
-export default scalarTest;
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,19 @@
 import mongo from "./clients/mongo.js";
-import scalarMongo from "./benchmarks/mongo/scalars.js";
+import * as benchmark from "benchmark";
 
-try {
-  console.log("teste");
-  scalarMongo(mongo);
-} finally {
-  mongo.close();
-}
+const suite = new benchmark.Suite();
+const db = mongo.db("document_db");
+
+suite
+  .add("Mongo#TotalAmountGreaterThan", function () {
+    db.collection("orders").find({ totalAmount: { $gt: 15000_00 } });
+  })
+  .on("cycle", function (event) {
+    console.log(String(event.target));
+  })
+  .on("complete", function () {
+    console.log("Fastest is " + (this)?.filter("fastest").map("name"));
+  })
+  .run({ async: true });
+
+// mongo.close();
